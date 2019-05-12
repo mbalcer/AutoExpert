1c02810b7d375322fef59854c8f3fc3369e87f8e
mbalcer <mateusz75319@gmail.com>
2019-05-12 22:43:45 +0200

    Added Hibernate configuration

---
+++ src/main/java/db/HibernateFactory.java
@@ -0,0 +1,17 @@
+package db;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateFactory {
+
+    public SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration().configure();
+        configuration.addAnnotatedClass(entity.Car.class);
+        StandardServiceRegistryBuilder registryBuilder =
+                new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        SessionFactory sessionFactory = configuration.buildSessionFactory(registryBuilder.build());
+        return sessionFactory;
+    }
+}
