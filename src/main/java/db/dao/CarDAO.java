c2ec14303de8b339f7d70ee8fa29ddc08ac3f8c0
mbalcer <mateusz75319@gmail.com>
2019-05-13 21:19:09 +0200

    Added readAllByCarClass method

--- src/main/java/db/dao/CarDAO.java
+++ src/main/java/db/dao/CarDAO.java
@@ -3,11 +3,15 @@ package db.dao;
 import db.HibernateFactory;
 import entity.Car;
 import org.hibernate.Session;
+import org.hibernate.criterion.CriteriaQuery;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+
 import java.util.List;
 
 public class CarDAO {
 
-    public List<Car> searchCar(Car searchedCar) {
+    public List<Car> search(Car searchedCar) {
         Session session = new HibernateFactory().getSessionFactory().openSession();
         List<Car> carList = session.createQuery("FROM Car WHERE carClass=:carClass AND engineCapacity=:eCapacity AND engineType=:eType AND enginePower=:ePower AND fuelUsageTo<=:fuelUsage")
                 .setParameter("carClass", searchedCar.getCarClass())
@@ -15,6 +19,16 @@ public class CarDAO {
                 .setParameter("eType", searchedCar.getEngineType())
                 .setParameter("ePower", searchedCar.getEnginePower())
                 .setParameter("fuelUsage", searchedCar.getFuelUsageTo())
+                .list();
+        session.close();
+
+        return carList;
+    }
+
+    public List<Double> readAllByCarClass(Character carClass) {
+        Session session = new HibernateFactory().getSessionFactory().openSession();
+        List<Double> carList = session.createQuery("SELECT DISTINCT engineCapacity FROM Car WHERE carClass=:carClass ORDER BY engineCapacity")
+                .setParameter("carClass", carClass)
                 .list();
 
         session.close();
