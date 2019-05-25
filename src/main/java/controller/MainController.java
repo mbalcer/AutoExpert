990db7a083f279158e4f5338597740a9db89b7a0
mbalcer <mateusz75319@gmail.com>
2019-05-25 10:39:48 +0200

    Loading welcome view to app screen

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,11 +1,37 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class MainController {
 
     @FXML
     private BorderPane mainBorderPane;
+
+    public void setMainBorderPane(Parent parent) {
+        mainBorderPane.setCenter(parent);
+    }
+
+    public void initialize() {
+        loadWelcomeView("/view/welcomeView.fxml");
+    }
+
+    public void loadWelcomeView(String viewPath) {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource(viewPath));
+        Parent parent = null;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        WelcomeController welcomeController = new WelcomeController();
+        welcomeController.setMainController(this);
+        setMainBorderPane(parent);
+    }
 
 }
