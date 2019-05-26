be651562636231838d43e14b6e205579f38c8dae
mbalcer <mateusz75319@gmail.com>
2019-05-26 10:09:16 +0200

    Created methods to reduce list by engine capacity and engine type

--- src/main/java/utility/AutoExpert.java
+++ src/main/java/utility/AutoExpert.java
@@ -23,4 +23,33 @@ public class AutoExpert {
                 .filter(car -> car.getCarClass() == condition)
                 .collect(Collectors.toList());
     }
+
+    public void reduceListByEngineType(String condition) {
+        carList = carList.stream()
+                .filter(car -> car.getEngineType().equals(condition.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    public void reduceListByEngineCapacity(Double rangeFrom, Double rangeTo) {
+        carList = carList.stream()
+                .filter(car -> car.getEngineCapacity()>=rangeFrom)
+                .filter(car -> car.getEngineCapacity()<=rangeTo)
+                .collect(Collectors.toList());
+    }
+
+    public Double[] setRangeEngineCapacity(String engineCapacity) {
+        Double[] range = new Double[2];
+        if (engineCapacity.equals("NISKA")){
+            range[0] = 0.8;
+            range[1] = 1.2;
+        } else if (engineCapacity.equals("ŚREDNIA")) {
+            range[0] = 1.2;
+            range[1] = 1.6;
+        } else if (engineCapacity.equals("WYSOKA")) {
+            range[0] = 1.6;
+            range[1] = 2.0;
+        }
+
+        return range;
+    }
 }
