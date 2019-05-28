b82cfcab2742aab760967652d8bfe47d5dd1c787
mbalcer <mateusz75319@gmail.com>
2019-05-28 18:19:08 +0200

    Buttons handling on result view

--- src/main/java/controller/ResultController.java
+++ src/main/java/controller/ResultController.java
@@ -2,11 +2,14 @@ package controller;
 
 import entity.Car;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import utility.AutoExpert;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 
@@ -46,7 +49,22 @@ public class ResultController {
 
     @FXML
     void exitApplication() {
+        System.exit(0);
+    }
 
+    @FXML
+    void loadStartView() {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/view/welcomeView.fxml"));
+        Parent parent = null;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        WelcomeController welcomeController = loader.getController();
+        welcomeController.setMainController(mainController);
+        mainController.setMainBorderPane(parent);
     }
 
 }
